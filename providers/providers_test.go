51eb57af96fdbb1a9c73caba7d954e09ae61194b
siamak a.m.o <edu.siamak@gmail.com>
2024-08-24 11:53:45 +0330

    update tests after cleanup

--- providers/providers_test.go
+++ providers/providers_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (p Provider) Equal(q Provider) bool {
-	if p.ID == q.ID &&
+	if p.id == q.id &&
 		p.Name == q.Name {
 		return true
 	}
@@ -14,11 +14,11 @@ func (p Provider) Equal(q Provider) bool {
 
 func Test__CDNs_integrity(t *testing.T) {
 	for idx, c := range CDNs {
-		if int(c.ID) != idx {
+		if int(c.id) != idx {
 			t.Errorf("%s\n%s\nCDNs[%v]->ID  !=  %v\n",
 				"CDNs data integrity test falied",
 				"make sure the ID of Providers is equal to their index in the CDNs array",
-				c.ID, idx)
+				c.id, idx)
 		}
 	}
 }
