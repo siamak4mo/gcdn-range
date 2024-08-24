8db5568edfa6fd22c72c551dc433fdd876621b4f
siamak a.m.o <edu.siamak@gmail.com>
2024-08-24 15:42:20 +0330

    we must use string channel of providers

--- cmd/download.go
+++ cmd/download.go
@@ -6,7 +6,10 @@ import (
 	"os"
 )
 
+type Dl_Writer func(*providers.Provider)
+
 type Downloader struct {
+	Out   Dl_Writer
 	Provs []*providers.Provider
 }
 
@@ -19,18 +22,20 @@ func (dl *Downloader) Init() *Downloader {
 	return dl
 }
 
-func (dl *Downloader) do() {
+func (dl *Downloader) Do() *Downloader {
 	for _, p := range dl.Provs {
-		p.DoFetch()
+		p.CIDR = make(providers.ProvChan)
+		go p.DoFetch()
+		dl.Out(p)
 		if p.DLerr != nil {
 			fmt.Fprintf(os.Stderr, "Could not download %s -- %v\n", p.Name, p.DLerr)
 		}
 	}
+	return dl
 }
 
 func (dl *Downloader) DL_all() *Downloader {
 	dl.Provs = providers.MKProvs()
-	dl.do()
 	return dl
 }
 
@@ -38,9 +43,7 @@ func (dl *Downloader) DL_name(name string) *Downloader {
 	p, e := providers.SearchCDN(name)
 	dl.Provs = []*providers.Provider{&p}
 
-	if e == nil {
-		dl.do()
-	} else {
+	if e != nil {
 		dl.Provs[0].DLerr = e
 		fmt.Fprintf(os.Stderr, "%s -- %v\n", name, e.Error())
 	}
@@ -49,6 +52,5 @@ func (dl *Downloader) DL_name(name string) *Downloader {
 
 func (dl *Downloader) DL_names(names []string) *Downloader {
 	dl.Provs = providers.MkProv(names)
-	dl.do()
 	return dl
 }
--- cmd/output.go
+++ cmd/output.go
@@ -1,31 +1,29 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"gcdn_range/providers"
+)
 
-func (dl *Downloader) Raw_stdout() *Downloader {
-	for _,p := range dl.Provs {
-		for _,cidr := range p.CIDR {
-			fmt.Println(cidr)
-		}
-	}
+func (dl *Downloader) SetOut(dw Dl_Writer) *Downloader {
+	dl.Out = dw
 	return dl
 }
 
-func (dl *Downloader) Formatted_stdout() *Downloader {
-	for _,p := range dl.Provs {
-		if p.DLerr != nil {
-			continue
-		}
-		fmt.Printf("%v:\n", p.Name)
-		if len(p.CIDR) == 0 {
-			fmt.Println("  - Empty")
-			continue
-		}
-		for _,cidr := range p.CIDR {
-			fmt.Println(cidr)
-		}
+func Raw_stdout(p *providers.Provider) {
+	for cidr := range p.CIDR {
+		fmt.Println(cidr)
 	}
-	return dl
+}
+
+func Formatted_stdout(p *providers.Provider) {
+	if p.DLerr != nil {
+		return
+	}
+	fmt.Printf("%v:\n", p.Name)
+	for cidr := range p.CIDR {
+		fmt.Printf("  - %s\n", cidr)
+	}
 }
 
 // TODO:  file output
